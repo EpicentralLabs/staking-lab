2fe9ec7cf8db9c706806c273f7807caeca659262
TheLazySol <tbelonog25@gmail.com>
2025-06-20 17:09:13 -0400

    Update RPC URLs in constants.ts for mainnet and devnet

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,12 +2,12 @@ import { createSolanaClient } from "gill";
 
 export const LABS_TOKEN_MINT = "LABSh5DTebUcUbEoLzXKCiXFJLecDFiDWiBGUU1GpxR";
 export const XLABS_TOKEN_MINT = "";
-export const MAINNET_RPC_URL = "";
+export const MAINNET_RPC_URL = "https://api.mainnet-beta.solana.com ";
 
 export const TEST_TOKEN_MINT = "2edbfZ4FdrkSrxYDsZEwVHP2QfFpbuXpgtfHgJA1G2pg";
 export const xTEST_TOKEN_MINT = "11111111111111111111111111111111";
 
-export const DEVNET_RPC_URL = "";
+export const DEVNET_RPC_URL = "https://api.devnet.solana.com";
 
 // APY configuration (in percentage)
 export const STAKE_APY = 10; // 10% APY
