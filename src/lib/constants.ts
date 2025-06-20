d0659d41d9c39ce23d105027d2690f2790b2e033
TheLazySol <tbelonog25@gmail.com>
2025-06-20 17:12:12 -0400

    Update XLABS_TOKEN_MINT constant in constants.ts to set a default mint address

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 import { createSolanaClient } from "gill";
 
 export const LABS_TOKEN_MINT = "LABSh5DTebUcUbEoLzXKCiXFJLecDFiDWiBGUU1GpxR";
-export const XLABS_TOKEN_MINT = "";
+export const XLABS_TOKEN_MINT = "11111111111111111111111111111111";
 export const MAINNET_RPC_URL = "https://api.mainnet-beta.solana.com ";
 
 export const TEST_TOKEN_MINT = "2edbfZ4FdrkSrxYDsZEwVHP2QfFpbuXpgtfHgJA1G2pg";
