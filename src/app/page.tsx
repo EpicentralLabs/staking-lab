6f4c6e61497cfe0a1781adf27754aca54a27915d
Pen <8bitpen@gmail.com>
2025-09-07 17:48:59 +0200

    comments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { DashboardFeature } from '@/components/dashboard/dashboard-feature'
-
+// TODO A better home page, and the toast should be styled like everything else. Also tailwind everything
 export default function Home() {
   return <DashboardFeature />
 }
