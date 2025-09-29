a447c49028832065e1a2588ff7fbb59327f13cc9
TheLazySol <thelazysol@gmail.com>
2025-09-28 22:49:01 -0400

    fix(prisma): optimize PrismaClient instantiation for better performance
    - Refactored PrismaClient initialization to use a global instance, reducing unnecessary re-instantiation during development.
    - Ensured that the global instance is only set in non-production environments, improving resource management and performance.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from '@prisma/client'
-import { withAccelerate } from '@prisma/extension-accelerate'
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
